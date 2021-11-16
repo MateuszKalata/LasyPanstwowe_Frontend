9870037c88dfb404432cc0b84ea3a4cfaabe15a2
Salabim-mb <m.balas1504@gmail.com>
2021-11-16 23:19:03 +0100

    polaczenie sensor-details z api

--- src/app/models/sensor.model.ts
+++ src/app/models/sensor.model.ts
@@ -1,12 +1,12 @@
 import { GeolocationModel } from './geolocation.model';
 
 export interface XSensor {
-  id: number;
+  id: number|string;
   name: string;
   dateAdded: string;
   geolocation: GeolocationModel;
   value: string;
-  forest_area_id?: number;
+  forest_area_id?: string|number;
   administrator?: string;
   status?: string;
   unit?: string;
